3b03063aa0700e2a936f84f08c82a8ddb07d6059
Tolan Blundell <tolan@patternseek.net>
2021-05-18 17:00:03 +0200

    MVP

---
+++ src/config.rs
@@ -0,0 +1,50 @@
+use regex::Regex;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::error::Error;
+use std::fs::File;
+use std::io::Read;
+use url::Url;
+
+pub fn load(config_path: String) -> Result<ConfigFile, Box<dyn Error>> {
+    let mut file = File::open(config_path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(serde_yaml::from_str(&contents)?)
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ConfigFile {
+    pub file_sets: HashMap<String, FileSetConfig>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct FileSetConfig {
+    pub file_globs: Vec<String>,
+    pub monitors: HashMap<String, MonitorConfig>,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct MonitorConfig {
+    #[serde(with = "serde_regex")]
+    pub regex: Regex,
+    pub log_recent_events: Option<usize>,
+    pub keep_lines_before: Option<usize>,
+    pub keep_lines_after: Option<usize>,
+    pub log_counts: bool,
+    pub notifiers: Option<Vec<NotifierConfig>>,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+#[serde(rename_all = "snake_case")]
+pub enum NotifierConfig {
+    Webhook(WebhookNotifierConfig),
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct WebhookNotifierConfig {
+    pub(crate) url: Url,
+    pub(crate) template: String,
+    pub(crate) minimum_interval: Option<usize>,
+    pub(crate) wait_seconds_for_additional_lines: Option<usize>,
+}
