41dc76ccc8e8a87d6c67891afba0efe41faa51ba
Tolan Blundell <tolan@patternseek.net>
2021-07-16 20:58:50 +0200

    Big refactor into tokio tasks and threads

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ mod core;
 mod data;
 mod notifiers;
 
-use crate::core::FileSet;
+use crate::config::{ConfigFile, NotifierConfig};
+use crate::core::{FileSet, FileSetId, Monitor, MonitorId};
+use crate::data::FileSetData;
+use crate::notifiers::{Notifier, NotifierId, WebhookBackEnd};
 use futures::future::{join_all, BoxFuture};
-use futures::prelude::*;
+use linemux::MuxedLines;
 use std::collections::HashMap;
-use std::error::Error;
 use std::process::exit;
 use structopt::*;
 
@@ -36,26 +38,77 @@ async fn main() -> std::io::Result<()> {
         }
     };
 
-    let mut filesets: HashMap<String, FileSet> = Default::default();
-    for (fileset_name, fileset_conf) in config.file_sets {
-        filesets.insert(fileset_name.clone(), FileSet::new_from_config(fileset_conf));
+    let (mut filesets, filesets_data, _monitors, notifiers) = pop_structs_from_config(config);
+
+    let notifiers_tx = notifiers::start_thread(notifiers);
+
+    let data_store_tx = data::start_task(filesets_data, notifiers_tx.clone());
+
+    let mut file_handler_futures: Vec<BoxFuture<()>> = Vec::new();
+
+    for (fileset_id, file_set) in &mut filesets {
+        let line_follower: MuxedLines = match file_set.get_follower().await {
+            Ok(lf) => lf,
+            Err(e) => {
+                eprintln!("Error: {}", e);
+                exit(1);
+            }
+        };
+        let fut = file_set.line_handler(fileset_id, line_follower, data_store_tx.clone());
+        file_handler_futures.push(Box::pin(fut));
     }
 
-    let mut file_handler_futures: Vec<BoxFuture<Result<(), Box<dyn Error>>>> = Vec::new();
-
-    for (file_set_name, file_set) in &mut filesets {
-        let fut = file_set.follow(file_set_name);
-        file_handler_futures.push(Box::pin(fut.boxed()));
-    }
-
-    let res = join_all(file_handler_futures).await;
-
-    res.iter().for_each(|res| match res {
-        Ok(_res) => (),
-        Err(err) => {
-            eprintln!("Error: {}", err);
-        }
-    });
+    let _res = join_all(file_handler_futures).await;
 
     Ok(())
+}
+
+fn pop_structs_from_config(
+    config: ConfigFile,
+) -> (
+    HashMap<FileSetId, FileSet>,
+    HashMap<FileSetId, FileSetData>,
+    HashMap<MonitorId, Monitor>,
+    HashMap<NotifierId, Notifier>,
+) {
+    let mut monitors: HashMap<MonitorId, Monitor> = Default::default();
+    for (monitor_id, monitor_config) in config.monitors {
+        monitors.insert(monitor_id.clone(), Monitor::new_from_config(monitor_config));
+    }
+
+    let mut filesets: HashMap<FileSetId, FileSet> = Default::default();
+    let mut filesets_data: HashMap<FileSetId, FileSetData> = Default::default();
+    for (fileset_id, fileset_conf) in config.file_sets {
+        // Create the FileSet
+        let fs = FileSet::new_from_config(fileset_conf, &monitors);
+        // Create a FileSetData for the FileSet
+        let mut fsd = FileSetData {
+            monitor_data: Default::default(),
+        };
+        // Create a MonitorData for each Monitor that's used by the FileSet
+        for (monitor_id, (_, _)) in &fs.monitor_notifier_sets {
+            fsd.monitor_data
+                .insert(monitor_id.clone(), Default::default());
+        }
+        filesets.insert(fileset_id.clone(), fs);
+        filesets_data.insert(fileset_id.clone(), fsd);
+    }
+
+    let mut notifiers: HashMap<NotifierId, Notifier> = Default::default();
+    for (notifier_id, notifier_config) in config.notifiers {
+        notifiers.insert(
+            notifier_id.clone(),
+            match &notifier_config {
+                NotifierConfig::Webhook(wh_config) => Notifier {
+                    config: notifier_config.clone(),
+                    back_end: Box::new(WebhookBackEnd {
+                        config: wh_config.clone(),
+                    }),
+                    last_notify: chrono::offset::Utc::now() - chrono::Duration::weeks(52),
+                    skipped_notifications: 0,
+                },
+            },
+        );
+    }
+    (filesets, filesets_data, monitors, notifiers)
 }
