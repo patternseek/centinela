9430b7f2ede2e87e38c7c3f36110ab7778380e44
Tolan Blundell <tolan@patternseek.net>
2025-02-21 18:05:41 +0100

    Back out "attempt to fix off by one error on previous lines"
    This backs out commit bf2dd568a903d6a71b6b67ae11f0931f1104cdc6.

--- src/fileset.rs
+++ src/fileset.rs
@@ -214,7 +214,7 @@ impl FileSet {
                 line: line.line().to_string(),
                 is_event_line: false,
             });
-            while buf.len() > self.max_lines_before +1  {
+            while buf.len() > self.max_lines_before {
                 buf.pop_front();
             }
         }
